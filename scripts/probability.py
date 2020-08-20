41a2f064c08c3412039c72f59b25f477c19c58ea
Bernardo <programming@bernardomg.com>
2020-08-21 01:57:33 +0200

    The function allows configuration

--- scripts/probability.py
+++ scripts/probability.py
@@ -13,22 +13,33 @@ def at_least_one_hit(to_hit, max_shots):
     return float(np.complex128(chance))
 
 
-def chance_above(floor, ceiling, goal):
+def roll_chance(floor, ceiling, goal, above=True, equal=False):
     """
-    Returns the chance to get a value above the goal.
-
-    The values should follow a homogeneous distribution. Each number should have the same chance of being selected.
+    Returns the chance to get a value above the goal. This is the same as rolling a dice and trying to get above a
+    value.
     """
 
     if ceiling == 0:
         chance = 0
-    elif goal >= ceiling:
+    elif goal > ceiling:
         chance = 0
     elif goal < floor:
         chance = 1
     else:
-        # Success is the inverse of the chance to fail
-        chance = goal / Decimal(ceiling - floor + 1)
-        chance = 1 - Decimal(chance)
+        if equal:
+            chance = (goal - 1) / Decimal(ceiling - floor + 1)
+        else:
+            chance = goal / Decimal(ceiling - floor + 1)
+
+        if chance > 1:
+            chance = 1
+        elif chance < 0:
+            chance = 0
+
+        if above:
+            # Success is the inverse of the chance to fail
+            chance = 1 - Decimal(chance)
+        else:
+            chance = Decimal(chance)
 
     return chance
