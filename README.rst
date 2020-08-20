902d52b5eb6011dd25c6f5440938171855785b8a
Bernardo <programming@bernardomg.com>
2020-08-20 20:59:16 +0200

    First version

---
+++ README.rst
@@ -0,0 +1,50 @@
+=================================
+Wargame Analysis Jupyter Notebook
+=================================
+
+Methodologies to analyze wargames.
+
+|binder|
+
+Usage
+-----
+
+Start the notebook with Jupyter:
+
+``jupyter notebook``
+
+Collaborate
+-----------
+
+Any kind of help with the project will be well received, and there are two main ways to give such help:
+
+- Reporting errors and asking for extensions through the issues management
+- or forking the repository and extending the project
+
+Issues management
+~~~~~~~~~~~~~~~~~
+
+Issues are managed at the GitHub `project issues tracker`_, where any Github
+user may report bugs or ask for new features.
+
+Getting the code
+~~~~~~~~~~~~~~~~
+
+If you wish to fork or modify the code, visit the `GitHub project page`_, where
+the latest versions are always kept. Check the 'master' branch for the latest
+release, and the 'develop' for the current, and stable, development version.
+
+License
+-------
+
+The project has been released under the `MIT License`_.
+
+.. _Coveralls: https://coveralls.io
+.. _GitHub project page: https://github.com/Bernardo-MG/wargame_analysis_jupyter_notebook
+.. _project issues tracker: https://github.com/Bernardo-MG/wargame_analysis_jupyter_notebook/issues
+.. _MIT License: http://www.opensource.org/licenses/mit-license.php
+
+.. |binder| image:: http://mybinder.org/badge.svg
+    :alt: Binder
+    :scale: 100%
+    :target: http://mybinder.org/repo/Bernardo-MG/wargame_analysis_jupyter_notebook
---
+++ setup.py
@@ -0,0 +1,74 @@
+# -*- coding: utf-8 -*-
+import io
+from os.path import dirname
+from os.path import join
+
+from setuptools import find_packages, setup
+
+from tox_test_command import ToxTestCommand
+from sphinx.setup_command import BuildDoc
+from version_extractor import extract_version_init
+
+
+"""
+PyPI configuration module.
+
+This is prepared for easing the generation of deployment files.
+"""
+
+__license__ = 'MIT'
+
+# Source package
+_source_package = 'dice_notation/'
+
+# Test requirements
+_tests_require = ['tox']
+
+
+# Gets the long description from the readme
+def read(*names, **kwargs):
+    return io.open(
+        join(dirname(__file__), *names),
+        encoding=kwargs.get('encoding', 'utf8')
+    ).read()
+
+
+setup(
+    name='wargame-analysis',
+    packages=find_packages(),
+    include_package_data=True,
+    package_data={
+    },
+    version=extract_version_init(_source_package),
+    description='Wargame Analysis',
+    author='Bernardo Martínez Garrido',
+    author_email='programming@bernardomg.com',
+    license='MIT',
+    url='https://github.com/Bernardo-MG/wargame_analysis_jupyter_notebook',
+    download_url='https://pypi.python.org/pypi/wargame_analysis',
+    keywords=['dice', 'dice notation', 'rpg', 'parser'],
+    platforms='any',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Topic :: Games/Entertainment :: Role-Playing'
+    ],
+    long_description=read('README.rst'),
+    install_requires=[
+        'bernardomg.tox-test-command',
+        'bernardomg.version-extractor'
+    ],
+    tests_require=_tests_require,
+    extras_require={'test': _tests_require},
+    cmdclass={
+        'build_docs': BuildDoc,
+        'test': ToxTestCommand
+    },
+    python_requires='>=3.6',
+)
