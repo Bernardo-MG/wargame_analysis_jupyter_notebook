770e5ab9c9d8f2cd6e51a26a2b198ff62d7e1661
Bernardo <programming@bernardomg.com>
2020-08-21 23:39:39 +0200

    Corrected calculation and added tests

--- scripts/probability.py
+++ scripts/probability.py
@@ -19,27 +19,43 @@ def roll_chance(floor, ceiling, goal, above=True, equal=False):
     value.
     """
 
+    # Data will normalized to start at 1
+    if floor <= 0:
+        diff = 0 - floor + 1
+        floor += diff
+        ceiling += diff
+        goal += diff
+
     if ceiling == 0:
         chance = 0
     elif goal > ceiling:
-        chance = 0
-    elif goal < floor:
-        chance = 1
-    else:
-        if equal:
-            chance = (goal - 1) / Decimal(ceiling - floor + 1)
-        else:
-            chance = goal / Decimal(ceiling - floor + 1)
-
-        if chance > 1:
-            chance = 1
-        elif chance < 0:
-            chance = 0
-
         if above:
-            # Success is the inverse of the chance to fail
-            chance = 1 - Decimal(chance)
+            chance = 0
         else:
-            chance = Decimal(chance)
+            chance = 1
+    elif goal < floor:
+        if above:
+            chance = 1
+        else:
+            chance = 0
+    else:
+        if not above:
+            chance = 1 - roll_chance(floor, ceiling, goal, not above, not equal)
+        else:
+            if equal:
+                chance = (goal - 1) / Decimal(ceiling - floor + 1)
+            else:
+                chance = goal / Decimal(ceiling - floor + 1)
+
+            if chance > 1:
+                chance = 1
+            elif chance < 0:
+                chance = 0
+
+            if above:
+                # Success is the inverse of the chance to fail
+                chance = 1 - Decimal(chance)
+            else:
+                chance = Decimal(chance)
 
     return chance
