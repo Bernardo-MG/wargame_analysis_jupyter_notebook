32c85a4551ba9caaac2f6159b0b148d86af5d475
Bernardo <programming@bernardomg.com>
2020-08-28 23:18:22 +0200

    Removed unused operation

--- scripts/probability.py
+++ scripts/probability.py
@@ -1,5 +1,5 @@
 from decimal import Decimal
-from scipy.stats import binom
+from scipy.stats import binom, randint
 
 
 def at_least_one(chance, attempts):
@@ -10,54 +10,6 @@ def at_least_one(chance, attempts):
         chance = 1
 
     return binom.cdf(attempts, attempts, chance) - binom.cdf(0, attempts, chance)
-
-
-def success_chance(floor, ceiling, goal, above=True, equal=False):
-    """
-    Returns the chance to get a value above the goal. This is the same as rolling a dice and trying to get above a
-    value.
-    """
-
-    # Data will normalized to start at 1
-    if floor <= 0:
-        diff = 0 - floor + 1
-        floor += diff
-        ceiling += diff
-        goal += diff
-
-    if ceiling == 0:
-        chance = 0
-    elif goal > ceiling:
-        if above:
-            chance = 0
-        else:
-            chance = 1
-    elif goal < floor:
-        if above:
-            chance = 1
-        else:
-            chance = 0
-    else:
-        if not above:
-            chance = 1 - success_chance(floor, ceiling, goal, not above, not equal)
-        else:
-            if equal:
-                chance = (goal - 1) / Decimal(ceiling - floor + 1)
-            else:
-                chance = goal / Decimal(ceiling - floor + 1)
-
-            if chance > 1:
-                chance = 1
-            elif chance < 0:
-                chance = 0
-
-            if above:
-                # Success is the inverse of the chance to fail
-                chance = 1 - Decimal(chance)
-            else:
-                chance = Decimal(chance)
-
-    return chance
 
 
 def roll_success_range(floor, ceiling, goal, above=True, equal=False, normalize=False):
